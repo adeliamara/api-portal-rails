799c8c832b7b1b442eb092f969659f9d34197f5f
Adélia Mara <adeliamara13@gmail.com>
2023-10-09 02:47:06 -0300

    fix: changes origins in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' # Ou qualquer outra origem que você esteja usando
+    origins 'https://portal-react-vemh.vercel.app/' # Ou qualquer outra origem que você esteja usando
     
     resource "*",
       headers: :any,
